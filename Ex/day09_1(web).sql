682ec5577cdd662e5a123370abe276708f692dce
cheoho-hi <cheoho-hi@DESKTOP-0HKEG65>
2025-06-10 14:16:33 +0900

    20250610(1)

--- Ex/day09_1(web).sql
+++ Ex/day09_1(web).sql
@@ -4,23 +4,24 @@ drop table book;
 
 -- 테이블 생성
 create table book(
-	book_id   int 			primary key 	auto_increment
-    ,title 	  varchar(100) 	not null
-    ,pubs 	  varchar(100)
-    ,pub_date datetime
+	book_id    int 				primary key 	auto_increment
+    ,title 	   varchar(100) 	not null
+    ,pubs 	   varchar(100)
+    ,pub_date  datetime
+    ,author_id int
 );
 
 -- 테이블 리스트 조회
 show tables;
 
 -- 책 데이터 입력
-insert into book values(null, '우리들의 일그러진 영웅', '다림', '1998-02-22');
-insert into book values(null, '삼국지', '민음사', '2002-03-01');
-insert into book values(null, '토지', '마로니애북스', '2012-08-15');
-insert into book values(null, '유시민의 글쓰기 특강', '생각의 길', '2015-04-01');
-insert into book values(null, '패션왕', '중앙북스(books)', '2012-02-22');
-insert into book values(null, '순정만화', '재미주의', '2011-08-03');
-insert into book values(null, '26년', '재미주의', '2012-02-04');
+insert into book values(null, '우리들의 일그러진 영웅', '다림', '1998-02-22', 1);
+insert into book values(null, '삼국지', '민음사', '2002-03-01', 1);
+insert into book values(null, '토지', '마로니애북스', '2012-08-15', 2);
+insert into book values(null, '유시민의 글쓰기 특강', '생각의 길', '2015-04-01', 3);
+insert into book values(null, '패션왕', '중앙북스(books)', '2012-02-22', 4);
+insert into book values(null, '순정만화', '재미주의', '2011-08-03', 5);
+insert into book values(null, '26년', '재미주의', '2012-02-04', 5);
 
 
 -- 데이터 조회
@@ -39,7 +40,7 @@ from    book b
 select * from book;
 
 -- 작가 데이터 추가
-insert into book values(null, '', '', '');
+insert into book values(null, '', '', '','');
 
 -- 작가 데이터 삭제
 delete from	book
@@ -50,4 +51,5 @@ update book
 set    title = ''
 	   ,pubs = ''
 	   ,pub_date = ''
+	   ,author_id = ''
 where  book_id = 1;
